03efe387bb34dae9cfee5b05cc544e758a5a6a16
Arventill <filip.nowicki1999@gmail.com>
2022-11-30 18:21:49 +0100

    Add example of the room usage.

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -7,9 +7,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.room.Room
+import com.example.composeapp.db.BeerDatabase
+import com.example.composeapp.db.Constants.BEER_DATABASE
 import com.example.composeapp.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val beerDB : BeerDatabase by lazy {
+        Room.databaseBuilder(this,BeerDatabase::class.java,BEER_DATABASE)
+            .allowMainThreadQueries()
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,6 +32,15 @@ class MainActivity : ComponentActivity() {
                     App()
                 }
             }
+        }
+    }
+
+    private fun checkItem(){
+        if(beerDB.doa().getAllBeers().isNotEmpty()){
+            // something
+        }
+        else{
+            // something else
         }
     }
 }
